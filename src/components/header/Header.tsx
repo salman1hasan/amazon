42661af8b8d0f7145104bf1ecee98dba1bbcbeec
Salman Zuberi Hussain(Sal Hasan) <96196317+salman1hasan@users.noreply.github.com>
2023-07-26 10:28:10 -0400

    Add files via upload

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,9 +3,10 @@ import logo from "../../images/logo.png"
 
 const Header =() =>{
   return (
-  <div className="w-full h-20 bg">
-    <div>
-      <Image className="" src={logo} alt="logoImg"/>
+<div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
+<div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
+    <Image className="w-28 object-cover mt-1" src={logo} alt="logoImg" />
+      <p>hello</p>
        </div>
     </div>
     )
