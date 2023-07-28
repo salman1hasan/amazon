ce0438853d35e508d6b2b33b40fd41f0c5f2b8e6
Salman Zuberi Hussain(Sal Hasan) <96196317+salman1hasan@users.noreply.github.com>
2023-07-28 10:48:58 -0400

    Add files via upload

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image"
 import logo from "../../images/logo.png"
 import { SlLocationPin } from "react-icons/sl";
+import { HiOutlineSearch } from "react-icons/hi";
+
+
 
 const Header =() =>{
   return (
@@ -10,12 +13,26 @@ const Header =() =>{
             <Image className="w-28 object-cover mt-1" src={logo} alt="logoImg" />
        </div>
        <div className="px-2 border border-transparent hover:border-white cursor-point duration-200 flex items-center justify-center h-[70%]
-       hidden xl:inline-flex gap-1"></div>
+       hidden xl:inline-flex gap-1">
        <SlLocationPin/>
        <div className="text-xs">
         <p>Deliver to</p>
         <p className="text-white font-body">USA</p>
        </div>
+    </div>
+    <div className="flex-1 h-10 hidden md:inline-flex">
+      <input className="w-full h-full rounded-md px-2 placeholder:text-sm text-base text-black border-[3px] border-transparent outline-none focus-visible:border-amazon_yellow" 
+      type="text" 
+      placeholder="Search next_amazon-yt products"
+      />
+      <span className="w-12 h-full bg-amazon-yellow text-black text-2xl flex items-center justify-center absolute right-0 rounded-tr-md
+      rounded-br-md-md"> 
+      <HiOutlineSearch/>
+      </span>
+    </div>
+    <div>
+      <p>Hello,signin</p>
+    </div>
     </div>
     </div>
     )
