d3c3e2c99522ce0cd1bc0ac4f54584485ee370ff
Salman Zuberi Hussain(Sal Hasan) <96196317+salman1hasan@users.noreply.github.com>
2023-08-21 09:44:45 -0400

    Add files via upload

--- src/components/header/Banner.tsx
+++ src/components/header/Banner.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Carousel } from "react-responsive-carousel";
 
-const Banner = () =>{
-  return <div>Banner</div>
+
+const Banner = () => {
+  return (
+    <div className="relative">
+      <Carousel>
+        <div>
+          <img src="assets/1.jpeg"/>
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src="assets/2.jpeg"/>
+           <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src="assets/3.jpeg"/>
+           <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Banner;
--- src/components/header/BottomHeader.tsx
+++ src/components/header/BottomHeader.tsx
@@ -8,8 +8,8 @@ const BottomHeader = () =>{
       <p className="hidden md:inline-flex flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">Todays Deals</p>
       <p className="hidden md:inline-flex flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">Customer Service</p>
       <p className="hidden md:inline-flex flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">Gifts Card</p>
-      <p className="hidden md:inline-flex flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">Todays Deals</p>
-      <p className="hidden md:inline-flex flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">Todays Deals</p>
+      <p className="hidden md:inline-flex flex items-center gap-1 h-8 px-2 border border-transparent hover:border-white cursor-pointer duration-300">Sell</p>
+      <p className="hidden md:inline-flex flex items-center gap-1 h-8 px-2 border border-transparent hover:border-red-600 hover:text-red-400 text-amazon_yellow cursor-pointer duration-300">Sign out</p>
     </div>
 };
 
