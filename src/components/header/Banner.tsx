92235c08fcba903ef3dcc49516ebb5950c05bcb6
Salman Zuberi Hussain(Sal Hasan) <96196317+salman1hasan@users.noreply.github.com>
2023-08-22 22:04:04 -0400

    Update Banner.tsx

--- src/components/header/Banner.tsx
+++ src/components/header/Banner.tsx
@@ -1,23 +1,31 @@
 import { Carousel } from "react-responsive-carousel";
-
+import sliderImg_1 from "../../images/slider/sliderImg_1.jpg"
+import sliderImg_2 from "../../images/slider/sliderImg_2.jpg"
+import sliderImg_3 from "../../images/slider/sliderImg_3.jpg"
+import Image from "next/image";
 
 const Banner = () => {
   return (
     <div className="relative">
-      <Carousel>
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={3000}
+      >
         <div>
-          <img src="assets/1.jpeg"/>
-          <p className="legend">Legend 1</p>
+          <Image priority src={sliderImg_1} alt="sliderImg" />
         </div>
         <div>
-          <img src="assets/2.jpeg"/>
-           <p className="legend">Legend 2</p>
+          <Image src={sliderImg_2} alt="sliderImg" />
         </div>
         <div>
-          <img src="assets/3.jpeg"/>
-           <p className="legend">Legend 3</p>
+          <Image src={sliderImg_3} alt="sliderImg" />
         </div>
       </Carousel>
+      <div className="w-full h-40 bg-gradient-to-t from-gray-100 to-transparent absolute bottom-0 z-20"></div>
     </div>
   );
 };
